de0e8aaa67e583f39f2ffdc29e7a5a2c0c25d7d0
Daniel Mora <danielleonardomora.04@gmail.com>
2025-03-02 23:45:26 -0500

    Turing machine fixed

--- turing-machine/main.py
+++ turing-machine/main.py
@@ -1,33 +1,123 @@
 class TuringMachine:
-    def __init__(self, tape="", blank_symbol=" ", initial_state="", final_states=None, transition_function=None):
-        self.tape = list(tape)
-        self.blank_symbol = blank_symbol
-        self.head_position = 0
-        self.current_state = initial_state
-        self.final_states = final_states if final_states is not None else set()
-        self.transition_function = transition_function if transition_function is not None else {}
+    def __init__(self, tape):
+        valid_chars = set("01+-*/%^r()= ")
+        if any(char not in valid_chars for char in tape):
+            raise ValueError("Error: La cinta contiene caracteres no permitidos.")
+        
+        self.tape = list(tape) + [' ']  # Agregar espacio en blanco al final
+        self.head = 0
+        self.state = 'q0'
 
-    def step(self):
-        current_symbol = self.tape[self.head_position] if self.head_position < len(self.tape) else self.blank_symbol
-        action = self.transition_function.get((self.current_state, current_symbol))
+    def move(self, direction):
+        if direction == 'R':
+            self.head += 1
+        elif direction == 'L' and self.head > 0:
+            self.head -= 1
+    
+    def read_symbol(self):
+        return self.tape[self.head]
+    
+    def write_symbol(self, symbol):
+        self.tape[self.head] = symbol
+    
+    def execute(self):
+        while self.state != 'qf':
+            symbol = self.read_symbol()
+            
+            if self.state == 'q0':
+                if symbol in '01':
+                    self.move('R')
+                elif symbol in '+-*/%^r()':
+                    self.move('R')
+                elif symbol == '=':
+                    self.calculate_result()
+                    self.state = 'qf'
+                elif symbol == ' ':
+                    self.state = 'qf'
 
-        if action:
-            new_state, new_symbol, direction = action
-            if self.head_position < len(self.tape):
-                self.tape[self.head_position] = new_symbol
+    def calculate_result(self):
+        expression = ''.join(self.tape).strip()
+        if '=' in expression:
+            expression = expression.split('=')[0]
+        
+        result = self.evaluate_expression(expression)
+        result_bin = bin(result)[2:]  # Convertir a binario sin prefijo '0b'
+        self.tape = list(result_bin) + [' ']  # Sobreescribir la cinta
+        self.head = 0
+
+    def evaluate_expression(self, expression):
+        postfix = self.infix_to_postfix(expression)
+        return self.evaluate_postfix(postfix)
+
+    def infix_to_postfix(self, expression):
+        precedence = {'+': 1, '-': 1, '*': 2, '/': 2, '%': 2, '^': 3, 'r': 4}
+        output = []
+        stack = []
+        buffer = ''
+        
+        for char in expression:
+            if char in '01':
+                buffer += char
+            elif char == '(':
+                stack.append(char)
+            elif char == ')':
+                if buffer:
+                    output.append(int(buffer, 2))
+                    buffer = ''
+                while stack and stack[-1] != '(':
+                    output.append(stack.pop())
+                stack.pop()
             else:
-                self.tape.append(new_symbol)
+                if buffer:
+                    output.append(int(buffer, 2))
+                    buffer = ''
+                while stack and precedence.get(stack[-1], 0) >= precedence.get(char, 0):
+                    output.append(stack.pop())
+                stack.append(char)
+        
+        if buffer:
+            output.append(int(buffer, 2))
+        while stack:
+            output.append(stack.pop())
+        
+        return output
 
-            if direction == "R":
-                self.head_position += 1
-            elif direction == "L" and self.head_position > 0:
-                self.head_position -= 1
-
-            self.current_state = new_state
-
-    def run(self):
-        while self.current_state not in self.final_states:
-            self.step()
-
+    def evaluate_postfix(self, postfix):
+        stack = []
+        for token in postfix:
+            if isinstance(token, int):
+                stack.append(token)
+            else:
+                if token == 'r':
+                    a = stack.pop()
+                    stack.append(int(a ** 0.5))
+                else:
+                    b = stack.pop()
+                    a = stack.pop()
+                    if token == '+':
+                        stack.append(a + b)
+                    elif token == '-':
+                        stack.append(a - b)
+                    elif token == '*':
+                        stack.append(a * b)
+                    elif token == '/':
+                        stack.append(a // b if b != 0 else 0)
+                    elif token == '%':
+                        stack.append(a % b if b != 0 else 0)
+                    elif token == '^':
+                        stack.append(a ** b)
+        return stack[0] if stack else 0
+    
     def get_tape(self):
         return ''.join(self.tape).strip()
+
+# Ejemplos de uso:
+#tape_input = "1100+101-11="  # (12 + 5 - 3) = 14 -> 1110
+#tape_input = "((1001+11)%11)="  # (9+3)%3 = 0 -> 0
+#tape_input = "((101+11)*10/10)="  # ((5+3)*2/2) = 8 -> 1000
+
+tape_input = input("Ingrese la cinta: ")
+
+tm = TuringMachine(tape_input)
+tm.execute()
+print("Resultado en la cinta:", tm.get_tape())
